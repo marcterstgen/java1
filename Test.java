15d5903b27efb614851bfeca8e79b6abd73a9f3b
yanglong <874037310@qq.com>
2019-03-27 18:44:01 +0800

    1

---
+++ Test.java
@@ -0,0 +1,16 @@
+package a.b.c;
+
+
+
+class Test1{
+    public int a;
+    public  char c;
+    }
+
+
+public class Test {
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+        System.out.println(test1.a + "" + test1.c);
+    }
+    }
