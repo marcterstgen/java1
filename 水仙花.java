c3f0a21e772a173d008488705d7bf458832652aa
yanglong <44842404+marcterstgen@users.noreply.github.com>
2019-04-02 15:06:07 +0800

    打印水仙花数

---
+++ 水仙花.java
@@ -0,0 +1,15 @@
+package a.b.c;
+
+public class Test {
+    public static void main(String[] args) {
+        for (int i =100; i < 1000 ; i++){
+            int x = i % 10;
+            int y = i/10 % 10;
+            int z = i/100%10;
+            if(x*x*x+y*y*y+z*z*z == i){
+                System.out.println(i);
+            }
+        }
+    }
+}
+
