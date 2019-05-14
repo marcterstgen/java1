3a9ccaf4e381d5b3cc992278c0cc24aceafb667f
yanglong <44842404+marcterstgen@users.noreply.github.com>
2019-05-14 17:45:16 +0800

    Rename Test3.java to Test4.java

---
+++ Test4.java
@@ -0,0 +1,19 @@
+package a.b.c;
+class Orange implements IFruit {
+    @Override
+    public void eat() {
+        System.out.println("[Orange] 吃橘子 ");
+    }
+}
+class FruitFactory {
+    private FruitFactory() {}
+    public static IFruit getInstance(String className) {
+        if ("apple".equals(className)) {
+            return new Apple() ;
+
+        }else if ("orange".equals(className)) {
+            return new Orange() ;
+        }
+        return null ;
+    }
+}
